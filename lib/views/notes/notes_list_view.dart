173b0ec6c100caa341df7e2df92fdccab27d315b
Tran Huynh Trong Toan <toantht@thuanhai.com.vn>
2023-08-10 14:39:09 +0700

    chapter 34: Update existing note in CreateUpdateNoteView

--- lib/views/notes/notes_list_view.dart
+++ lib/views/notes/notes_list_view.dart
@@ -2,13 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:mynotes/services/crud/notes_service.dart';
 import 'package:mynotes/utilities/dialogs/delete_dialog.dart';
 
-typedef DeleteNoteCallback = void Function(DatabaseNote note);
+typedef NoteCallback = void Function(DatabaseNote note);
 
 class NotesListView extends StatelessWidget {
   final List<DatabaseNote> notes;
-  final DeleteNoteCallback onDeleteNote;
-  const NotesListView(
-      {super.key, required this.notes, required this.onDeleteNote});
+  final NoteCallback onDeleteNote;
+  final NoteCallback onTap;
+  const NotesListView({
+    super.key,
+    required this.notes,
+    required this.onDeleteNote,
+    required this.onTap,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +22,9 @@ class NotesListView extends StatelessWidget {
       itemBuilder: (context, index) {
         final note = notes[index];
         return ListTile(
+          onTap: () {
+            onTap(note);
+          },
           title: Text(
             note.text,
             maxLines: 1,
